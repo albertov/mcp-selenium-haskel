8b1ca96844e5f615cab29cb90b6e497de41417ed
Alberto Valverde <alberto@toscat.net>
2025-06-10 08:47:23 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
+  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.treefmt-nix = {
+    url = "github:numtide/treefmt-nix";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = inputs@{ nixpkgs, flake-utils, haskellNix, ... }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+    in
+    flake-utils.lib.eachSystem supportedSystems (system:
+      let
+        overlays = [
+          haskellNix.overlay
+          (final: _prev: {
+            hixProject =
+              final.haskell-nix.hix.project {
+                src = ./.;
+                evalSystem = "x86_64-linux";
+              };
+          })
+        ];
+        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+        flake = pkgs.hixProject.flake { };
+      in
+      flake // {
+        legacyPackages = pkgs;
+
+        packages = flake.packages // { default = flake.packages."mcp-selenium:exe:mcp-selenium-hs"; };
+
+        formatter = inputs.treefmt-nix.lib.mkWrapper pkgs {
+          projectRootFile = "flake.nix";
+          programs = {
+            nixpkgs-fmt.enable = true;
+            ormolu.enable = true;
+            deadnix.enable = true;
+            shellcheck.enable = true;
+          };
+        };
+      });
+
+  # --- Flake Local Nix Configuration ----------------------------
+  nixConfig = {
+    # This sets the flake to use the IOG nix cache.
+    # Nix should ask for permission before using it,
+    # but remove it here if you do not want it to.
+    extra-substituters = [ "https://cache.iog.io" ];
+    extra-trusted-public-keys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
+    allow-import-from-derivation = "true";
+  };
+}
---
+++ nix/hix.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }: {
+  # name = "project-name";
+  compiler-nix-name = "ghc967"; # Version of GHC to use
+
+  crossPlatforms = _p: pkgs.lib.optionals pkgs.stdenv.hostPlatform.isx86_64 ([
+  ] ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isLinux [
+    # p.musl64
+  ]);
+
+  # Tools to include in the development shell
+  shell.tools.cabal = "latest";
+  shell.tools.hlint = "latest";
+  shell.tools.ormolu = "latest";
+  # shell.tools.haskell-language-server = "latest";
+}
