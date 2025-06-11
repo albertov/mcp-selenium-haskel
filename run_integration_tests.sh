70c4a35e1e0ca3f096c23e16f291ed4bd5b172bd
Alberto Valverde <alberto@toscat.net>
2025-06-12 00:31:44 +0200

    make run_integration_tests.sh run cabal test too

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -4,6 +4,7 @@
 # IMPORTANT: Do not change the timeout!
 set -eu -o pipefail
 cabal build
+cabal test
 NEWPYTHONPATH="$(pwd)/integration_tests:$PYTHONPATH"
 export PYTHONPATH="${NEWPYTHONPATH}"
 exec timeout 90s python3 integration_tests/orchestrate_integration_tests.py
