1b90984ad0305b86c06f5f1f7912ab9ccd39d19f
Alberto Valverde <alberto@toscat.net>
2025-06-12 00:31:44 +0200

    make nix eval faster

--- nix/hix.nix
+++ nix/hix.nix
@@ -2,6 +2,9 @@
 rec {
   name = "mcp-selenium-haskell";
   compiler-nix-name = "ghc910"; # Version of GHC to use
+  index-state = "2025-06-09T20:31:03Z";
+  # This must be updated when haskell dependencies change
+  plan-sha256 = "sha256-jd+Kx7EB4EHPqqguQzjpr7PMhnpCm7Ortpy2Pvq1Oeg=";
 
   # Tools to include in the development shell
   shell.tools.cabal = "latest";
